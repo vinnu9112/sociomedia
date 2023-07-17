049b3bffc15124c5313a2339bd178f8d4a700d18
vinnu9112 <111413210+vinnu9112@users.noreply.github.com>
2023-07-17 16:24:54 +0530

    6th commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 import bcrypt, { compare } from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
-import mongoose from 'mongoose'
 
 
 export const register = async(req, res)=>{
